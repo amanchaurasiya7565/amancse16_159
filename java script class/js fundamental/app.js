d7033f9f822f3b9cf0f94264c02e85297c81e762
amanchaurasiya7565 <amanchaurasiya7565@gmail.com>
2026-04-16 09:22:43 +0530

    first commit

---
+++ java script class/js fundamental/app.js
@@ -0,0 +1,133 @@
+// var name="Aman Chaurasiya";
+// var age=21;
+// var isEmployed=true;
+// console.log(name);
+// console.log(age);
+// console.log(isEmployed);
+// var age="15";
+// console.log(age);
+// let name="Aman Chaurasiya";
+// let age="21";
+// let isEmployed="true";
+// console.log(name);
+// console.log(age);
+// console.log(isEmployed);
+// let age="15";
+// console.log(age)
+// if(true){
+//     var age=30;
+// }
+//   console.log(age);
+// if(true){
+//     let age=30;
+// }
+//   console.log(age);
+// const PI=3.14;
+// console.log(PI);
+// const PI=3.1459;
+// console.log(PI);
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isEmployed);
+// let name="Aman chaurasiya";
+// let age1=20;
+// console.log("My name is "+ name+" and my age is "+ age1);
+// console.log(`My name is ${name} and my age is ${age1}`);
+
+// let data=null;
+// console.log(data);
+// console.log(typeof data);
+
+// let result;
+// console.log(result);
+// console.log(typeof result);
+
+
+// let bigIntvalue = 12343546453475593488388382843745789344563554n;
+// console.log(typeof bigIntvalue);
+
+let sym1=symbol("sym");
+let sym2=symbol("sym");
+console.log(sym1);
+console.log(sym2);
+console.log(sym1===sym2);
+
+// let student={
+//     name:"Aman",
+//     age:20,
+//     isStudent: false
+// };
+
+
+// console.log(student);
+// console.log(student.name);
+// console.log(student["age"]);
+// let numbers=[1,2,3,4,5];
+// console.log(numbers);
+ 
+// let random=[1,"apple"];
+// console.log(random);
+// let arrnew=[1,2,3];
+// arrnew.push(4);
+// console.log(arrnew);
+
+// let arrnew=[1,2,3,4,5];
+// arrnew.pop();
+// console.log(arrnew);
+
+// let arr=[1,2,3,4,5];
+// arr.shift();
+// console.log(arr);
+
+// let arr=[1,2,3,4,5];
+// arr.unshift(0);
+// console.log(arr);
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// arr.forEach(function(element){
+//     console.log(element);
+// });
+
+// let squareArr =arr.map(function(element){
+//     return element*element;
+// });
+// console.log(squareArr);
+
+// let evenArr =arr.filter(function(element){
+//     return element % 2 ===0;
+// });
+ // console.log(evenArr);
+// let arr=[1,2,3,4,5];
+// let sum =arr.reduce(function(accumulator,currentValue){
+//     return accumulator + currentValue;
+// },0);
+// console.log(sum);
+
+// function greet(){
+//     console.log("Hello, Aman!");
+// }
+// greet();
+
+// function greet(name){
+//     console.log("Hello," + name + "!");
+// }
+// greet("Aman");
+
+// function add(a,b){
+//  return a+b;
+// }
+// let result =add(5,10);
+// console.log (result);
+
+// let greet2 =function(name){
+//     console.log("Hello," + name + "!");
+// };
+// greet2("BOb");
+//tamplet  literals
+// let name3 = "Charlie";
+// let greeting =`hello,${name3}!`;
+// console.log(greeting);
+
